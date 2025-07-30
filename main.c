5e4aa46da0d743c620c79ed32c374f58f16a20dd
Piotr Kubaj <pkubaj@FreeBSD.org>
2025-07-30 19:55:00 +0000

    Fix build on powerpc*
    --- main.o ---
    main.c:305:36: error: 'musttail' attribute for this call is impossible because external calls can not be tail called on PPC
      305 |                 __attribute__((musttail)) return openat_retry(dfd, path, flags, mode);
          |                                                  ^
    1 error generated.

--- main.c
+++ main.c
@@ -302,7 +302,11 @@ openat_retry(const int dfd, const char *const path, int flags, const mode_t mode
 	if (fd >= 0 || errno != EINTR) {
 		return fd;
 	} else {
+#ifdef __powerpc__
+		return openat_retry(dfd, path, flags, mode);
+#else
 		__attribute__((musttail)) return openat_retry(dfd, path, flags, mode);
+#endif
 	}
 }
 
